f39468c5f0797506119c6546e443d1ce78ac8ada
Gonzalo Benegas <gbenegas@berkeley.edu>
2025-02-06 17:58:51 -0800

    Add script to upload new model features to HF

---
+++ workflow/scripts/upload_model.py
@@ -0,0 +1,62 @@
+from huggingface_hub import HfApi
+import os.path
+import sys
+
+
+model = sys.argv[1]
+
+
+api = HfApi()
+repo_id = "songlab/TraitGym"
+
+
+def upload_file(path):
+    if os.path.exists(path):
+        api.upload_file(
+            path_or_fileobj=path, path_in_repo=path, repo_id=repo_id,
+            repo_type="dataset",
+        )
+
+
+datasets = [
+    "mendelian_traits_matched_9",
+    "complex_traits_matched_9",
+]
+
+metrics = [
+    "AUPRC_by_chrom",
+    "AUROC_by_chrom",
+    "AUPRC_by_chrom_weighted_average",
+    "AUROC_by_chrom_weighted_average",
+]
+
+features = [
+    "LLR",
+    "absLLR",
+    "Embeddings",
+    "InnerProducts",
+]
+
+preds = [
+    "_LLR.minus.score",
+    "_absLLR.plus.score",
+    "_Embeddings.plus.cosine_distance",
+    "_Embeddings.minus.inner_product",
+    "_Embeddings.plus.euclidean_distance",
+    ".LogisticRegression.chrom",
+    ".LogisticRegression.chrom.subset_from_all",
+]
+
+subsets = [
+    "all",
+    "no_cadd_overlap",
+]
+
+for dataset in datasets:
+    for feature in features:
+        upload_file(f"{dataset}/features/{model}_{feature}.parquet")
+    for subset in subsets:
+        for pred in preds:
+            upload_file(f"{dataset}/preds/{subset}/{model}{pred}.parquet")
+            for metric in metrics:
+                upload_file(f"{dataset}/{metric}/{subset}/{model}{pred}.csv")
